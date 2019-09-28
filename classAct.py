4c1d9c60fd06f11e6f07fb8f32c38135e9a89bf6
BakytMamytbekov <mamytbekov_b@auca.kg>
2019-09-28 20:00:17 +0600

    First commit

---
+++ classAct.py
@@ -0,0 +1,9 @@
+#data types char = 'a' char equals to 1 letter
+#boolean
+a=2
+b=3
+c=4
+print (a*(b+c))
+
+
+
---
+++ tasks.py
@@ -0,0 +1,54 @@
+a = 2
+b = 3
+c = 4
+print (a*(b+c))
+print (a*b+c)
+print (a+b*c)
+#4
+import math
+print (math.sqrt(25))
+#5
+a = int("1")
+#
+b = float("2.0")
+#
+c = int("3")
+#
+print (a+b+c)
+#6
+#
+a = 66.397
+#
+print (round(a))
+#7
+#
+a = 45
+#
+print (float(a))
+#8
+#
+import random
+#
+print(random.randint(1,100))
+#
+print(random.randint(1,100))
+#
+print(random.randint(1,100))
+#
+print(random.randint(1,100))
+#
+print(random.randint(1,100))
+#9
+#
+import math
+#
+print (math.sqrt(365))
+#10
+#
+import math
+#
+a = 65
+#
+print (math.pow(a,3))
+
+

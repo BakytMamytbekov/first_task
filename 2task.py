a115486ec184a13e75d0cb8ed1b40013353d7f4f
BakytMamytbekov <55870903+BakytMamytbekov@users.noreply.github.com>
2019-09-29 23:33:36 +0600

    Add files via upload

---
+++ 2task.py
@@ -0,0 +1,45 @@
+#Tasks
+#1
+##S ='Flower'
+##S3 = S[:3]
+##S4 = S[2:]
+##print (S3)
+##print (S4)
+#2
+##b = 'bla'
+##b3 = b*3
+##print (b3)
+#3
+##b = 'BEAUTIFUL'
+####print (b.lower())
+#4
+##a = input("Choose word:")
+##b = "Super"
+##print (a+" "+ b)
+#5
+##S = 'spam'
+####S1 = S[1:3]
+####S2 = S[1:-1]
+####print (S1)
+####print (S2)
+#6-not done
+##s = "ImSuccessaSuccesslumberjack"
+##t = s.split()
+##print (t)
+#7
+##user = input("Write something:")
+##b = user.isdigit()
+##c = user.islower()
+##d = user.isupper()
+##print (b,c,d)
+#8
+##xs = 'xxxxSuccessxxxxSuccessxxxx'
+##print (xs.replace('Success' , 'Apple'))
+#9
+##f = 'xxxxSuccessxxxxSuccessxxxx'
+##print (f.find('Success'))
+#10
+##f = 'Hello World'
+##print (len(f))
+
+

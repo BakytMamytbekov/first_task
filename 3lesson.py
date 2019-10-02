07c1fc8dd111112685dbe68c17f7a5bec6ac005d
BakytMamytbekov <55870903+BakytMamytbekov@users.noreply.github.com>
2019-10-02 23:46:24 +0600

    Add files via upload

---
+++ 3lesson.py
@@ -0,0 +1,87 @@
+#Task1
+'''
+bbb = open('bbb.txt', 'w')
+bbb.write('What"s up!\n')
+bbb.write('Everything is Great!\n')
+
+import json
+a = {
+    "Your name": "Bakyt",
+     "Your age": "21",
+    "your education level": "Bachelor's degree",
+    "Your city": "Bishkek"
+}
+print(json.dumps(a, indent=4))
+
+while True:
+    print('Hi')
+'''
+#Task2
+'''1
+q = int(input("Put your number:"))
+if q==0:
+   pass
+elif q==1:
+    print ("You successfully accomplished my task")
+else:
+   print("I want another number")
+'''
+
+'''2
+while True:
+    x = int(input("Put your number:"))
+    if x==7: break
+'''
+'''3
+a,b,c,d,e,f,g = 'Success'
+print (a,b,c,d,e,f,g)
+'''
+'''4
+x = 0
+while True:
+    intg = int(input("Your number:"))
+    if intg == 0: break
+    x += intg
+print (intg)
+'''
+'''5
+while True:
+    num =int(input("Enter your number: "))
+    if num == 1:
+        print('January')
+    if num ==2:
+        print('Febraury')
+    if num ==3:
+        print('March')
+    if num ==4:
+        print('April')
+    if num ==5:
+        print('May')
+    if num ==6:
+        print('June')
+    if num ==7:
+        print('July')
+    if num ==8:
+        print('August')
+    if num ==9:
+        print('September')
+    if num ==10:
+        print('October')
+    if num ==11:
+        print('November')
+    if num ==12:
+        print('December')
+    else:
+        print ("False")
+'''
+
+    
+       ' 
+        
+
+
+    
+    
+    
+
+    

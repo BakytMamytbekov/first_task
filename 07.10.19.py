c2d6338389b174029aa6d4c9ab750978abb3084b
BakytMamytbekov <55870903+BakytMamytbekov@users.noreply.github.com>
2019-10-08 00:50:27 +0600

    Add files via upload

---
+++ 07.10.19.py
@@ -0,0 +1,31 @@
+#07.10.19 For Loop
+'''#1
+countries = ["Canada", "USA", "Mexico"]
+for country in countries:
+    print (country, end=' ')
+#2
+for number in range(1,100):
+    print (number, 'Count')
+
+#3
+for x in range(1, 13):
+    for y in range(1,13):
+        print(y*x,end=' ')
+    print()
+
+#4
+for x in range(10, 0, -1):
+    print(x)
+
+#5
+for even in range(2,11):
+    if even % 2 == 0:
+        print(even)
+
+#6
+sum = 0
+for number in range (100,201):
+    sum = sum+number
+print(sum)
+'''  
+    
